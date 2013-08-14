2035777bdf5e3343900990d3a66a0953381f0c47
siman-man <k128585@ie.u-ryukyu.ac.jp>
2013-08-14 17:56:28 +0900

    canvasJs add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,28 @@ $(document).ready(function(){
     }
   });
 
+  var chart = new CanvasJS.Chart("chartContainer", {
+    title:{
+      text: "Fruits sold in First Quarter"              
+    },
+    data: [//array of dataSeries              
+      { //dataSeries object
+
+      /*** Change type "column" to "bar", "area", "line" or "pie"***/
+      type: "column",
+      dataPoints: [
+      { label: "banana", y: 18 },
+      { label: "orange", y: 29 },
+      { label: "apple", y: 40 },                                    
+      { label: "mango", y: 34 },
+      { label: "grape", y: 24 }
+      ]
+      }
+      ]
+  });
+
+    chart.render();
+
   var WS = Simulator;
   $( "div#master" ).slider({
     value: 0.0,
